551636f1564331474118ab1f5130d0d31bf9494a
MarcBComas <marcbarcelo2@gmail.com>
2022-11-04 16:37:09 +0100

    next release

--- app/src/main/java/com/example/appdam/MainActivity.java
+++ app/src/main/java/com/example/appdam/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.view.MotionEvent;
 import android.view.GestureDetector;
@@ -13,12 +15,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GestureDetectorCompat;
 
 public class MainActivity extends AppCompatActivity{
-
+    private LinearLayout mLayout;
+    private EditText mEditText;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLayout = (LinearLayout) findViewById(R.id.article_linearlayout);
+        mEditText = (EditText) findViewById(R.id.comment_editText);
+        mButton = (Button) findViewById(R.id.add_comment);
+        mButton.setOnClickListener(onClick());
+        TextView textView = new TextView(this);
+        textView.setText("New text");
     }
 
+    private View.OnClickListener onClick() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mLayout.addView(createNewTextView(mEditText.getText().toString()));
+            }
+        };
+    }
+
+    private TextView createNewTextView(String text) {
+        final LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        final TextView textView = new TextView(this);
+        textView.setLayoutParams(lparams);
+        textView.setText(text);
+        return textView;
+    }
 }
