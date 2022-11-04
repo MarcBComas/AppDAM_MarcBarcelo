4e1d565f56ef95be4f56e8af57327e1e05d92111
MarcBComas <marcbarcelo2@gmail.com>
2022-11-04 15:54:34 +0100

    next release

--- app/src/main/java/com/example/appdam/MainActivity.java
+++ app/src/main/java/com/example/appdam/MainActivity.java
@@ -13,26 +13,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GestureDetectorCompat;
 
 public class MainActivity extends AppCompatActivity{
-    private Button countButton;
-    private TextView countTextView;
-    private int numCount = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        countTextView = (TextView) findViewById(R.id.count_text_view);
-        countTextView.setText(numCount);
-        countButton = (Button) findViewById(R.id.count_button);
-        countButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                count();
-            }
-        });
-    }
-    private void count() {
-        countTextView.setText(numCount++);
     }
 
 }
