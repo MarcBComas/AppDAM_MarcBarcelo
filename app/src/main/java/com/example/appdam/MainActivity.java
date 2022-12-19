76e34466fbf310b22556789adb660b0edb927b8d
MarcBComas <marcbarcelo2@gmail.com>
2022-12-19 19:14:53 +0100

    next release

--- app/src/main/java/com/example/appdam/MainActivity.java
+++ app/src/main/java/com/example/appdam/MainActivity.java
@@ -11,12 +11,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.view.MotionEvent;
 import android.view.GestureDetector;
@@ -38,75 +43,32 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity{
-    private EditText link;
-    private Button download;
-    private TextView urlcontent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        link = (EditText) findViewById(R.id.ETurl);
-        download = (Button) findViewById(R.id.Bdownload);
-        urlcontent = (TextView) findViewById(R.id.linkresult);
-    }
-
-    public void downloadurl(View v) throws IOException {
-        ConnectivityManager con = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo nInfo = con.getActiveNetworkInfo();
-        if(nInfo !=null && nInfo.isConnected()){
-            new DownloadWebpageText().execute(link.getText().toString());
-        }else {
-            urlcontent.setText("No network connection available");
+        Animation fade1 = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        TextView logo1 = (TextView) findViewById(R.id.TextViewTopTitle);
+        TextView logo2 = (TextView) findViewById(R.id.TextViewBottomTitle);
+        logo1.startAnimation(fade1);
+        logo2.startAnimation(fade1);
+        Animation spinin = AnimationUtils.loadAnimation(this, R.anim.custom_anim);
+        LayoutAnimationController controller =
+                new LayoutAnimationController(spinin);
+        TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.setLayoutAnimation(controller);
         }
     }
 
-    private class DownloadWebpageText extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... url) {
-            try{
-                return downloadUrl(url[0]);
-            } catch (IOException e){
-                return "Unable to retrieve web page. URL may be invalid.";
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            urlcontent.setText(result);
-        }
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        TextView logo1 = (TextView) findViewById(R.id.TextViewTopTitle);
+        logo1.clearAnimation();
+        TextView logo2 = (TextView) findViewById(R.id.TextViewBottomTitle);
+        logo2.clearAnimation();
     }
-    private String downloadUrl(String myURL) throws IOException{
-        InputStream is = null;
-        try{
-            URL url = new URL(myURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setReadTimeout(10000);
-            conn.setConnectTimeout(15000);
-            conn.setRequestMethod("GET");
-            conn.setDoInput(true);
-            conn.connect();
-            is = conn.getInputStream();
-            String contentAsString = reader(is);
-            return contentAsString;
-
-
-        }finally {
-            if(is != null){
-                is.close();
-            }
-        }
-    }
-
-    public String reader(InputStream stream) throws IOException {
-        String textReturn = "";
-        int ch = stream.read();
-        while(ch!=-1){
-            textReturn = textReturn + Character.toString((char)ch);
-        }
-        return textReturn;
-    }
-
 }
