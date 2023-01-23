1208083f985dc51aa33598e5ebfc0f5c31138854
MarcBComas <marcbarcelo2@gmail.com>
2023-01-23 19:08:18 +0100

    next release

--- app/src/main/java/com/example/appdam/MainActivity.java
+++ app/src/main/java/com/example/appdam/MainActivity.java
@@ -8,8 +8,11 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -27,7 +30,9 @@ import android.view.MotionEvent;
 import android.view.GestureDetector;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.view.GestureDetectorCompat;
@@ -43,32 +48,91 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity{
-
+    TextView tvscore1;
+    TextView tvscore2;
+    int score1;
+    int score2;
+    Toast limit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Animation fade1 = AnimationUtils.loadAnimation(this, R.anim.fade_in);
-        TextView logo1 = (TextView) findViewById(R.id.TextViewTopTitle);
-        TextView logo2 = (TextView) findViewById(R.id.TextViewBottomTitle);
-        logo1.startAnimation(fade1);
-        logo2.startAnimation(fade1);
-        Animation spinin = AnimationUtils.loadAnimation(this, R.anim.custom_anim);
-        LayoutAnimationController controller =
-                new LayoutAnimationController(spinin);
-        TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
-        for (int i = 0; i < table.getChildCount(); i++) {
-            TableRow row = (TableRow) table.getChildAt(i);
-            row.setLayoutAnimation(controller);
+        tvscore1 = (TextView) findViewById(R.id.score1);
+        tvscore2 = (TextView) findViewById(R.id.score2);
+        if (savedInstanceState != null) {
+            score1 = savedInstanceState.getInt("STATE_SCORE_1");
+            score2 = savedInstanceState.getInt("STATE_SCORE_2");
+            tvscore1.setText(String.valueOf(score1));
+            tvscore2.setText(String.valueOf(score2));
         }
+        limit = Toast.makeText(this, "Limite alcanzado", Toast.LENGTH_SHORT);
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        TextView logo1 = (TextView) findViewById(R.id.TextViewTopTitle);
-        logo1.clearAnimation();
-        TextView logo2 = (TextView) findViewById(R.id.TextViewBottomTitle);
-        logo2.clearAnimation();
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        outState.putInt("STATE_SCORE1", score1);
+        outState.putInt("STATE_SCORE2", score2);
+        super.onSaveInstanceState(outState);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if(nightMode == AppCompatDelegate.MODE_NIGHT_YES){
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else{
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.night_mode){
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+            if(nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                recreate();
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                recreate();
+            }
+        }
+        return true;
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.plus1:
+                if (tvscore1.getText().toString().equals("100")) {
+                    limit.show();
+                } else {
+                    tvscore1.setText(String.valueOf(Integer.parseInt(tvscore1.getText().toString()) + 1));
+                }
+                break;
+            case R.id.plus2:
+                if (tvscore2.getText().toString().equals("100")) {
+                    limit.show();
+                } else {
+                    tvscore2.setText(String.valueOf(Integer.parseInt(tvscore2.getText().toString()) + 1));
+                }
+                break;
+            case R.id.minus1:
+                if (tvscore1.getText().toString().equals("0")) {
+                    limit.show();
+                } else {
+                    tvscore1.setText(String.valueOf(Integer.parseInt(tvscore1.getText().toString()) - 1));
+                }
+                break;
+            case R.id.minus2:
+                if (tvscore2.getText().toString().equals("0")) {
+                    limit.show();
+                } else {
+                    tvscore2.setText(String.valueOf(Integer.parseInt(tvscore2.getText().toString()) - 1));
+                }
+                break;
+        }
+    }
+
 }
